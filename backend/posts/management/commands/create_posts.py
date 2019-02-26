2028a504cc0459ef5b06b27982a2de3c2746eb0e
darkwoolf10 <woolftreuser@yandex.ru>
2019-02-26 23:51:14 +0200

    deleted excess

--- backend/posts/management/commands/create_posts.py
+++ backend/posts/management/commands/create_posts.py
@@ -1,25 +1,25 @@
-from posts.models import Post
-from django.core.management.base import BaseCommand
-
-import factory
-
-
-class PostFactory(factory.django.DjangoModelFactory):
-    class Meta:
-        model = Post
-    title = factory.Faker('name')
-    content = factory.Faker('bs')
+# from posts.models import Post
+# from django.core.management.base import BaseCommand
+#
+# import factory
 
 
-class Command(BaseCommand):
-    help = 'Creates dummy Posts to seed the database'
+# class PostFactory(factory.django.DjangoModelFactory):
+#     class Meta:
+#         model = Post
+#     title = factory.Faker('name')
+#     content = factory.Faker('bs')
 
-    def handle(self, *args, **options):
-        posts = Post.objects.all()
-        if not posts:
-            for i in range(1000):
-                post = PostFactory()
-                post.save()
-            print("Created posts")
-        else:
-            print("Not creating posts")
+
+# class Command(BaseCommand):
+#     help = 'Creates dummy Posts to seed the database'
+#
+#     def handle(self, *args, **options):
+#         posts = Post.objects.all()
+#         if not posts:
+#             for i in range(1000):
+#                 post = PostFactory()
+#                 post.save()
+#             print("Created posts")
+#         else:
+#             print("Not creating posts")
